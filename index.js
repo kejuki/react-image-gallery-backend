75e678016ff3d10190fde6de7df0996eb761bfac
kejuki <kerkkokiikeri97@gmail.com>
2023-08-16 15:00:22 +0300

    new dummy data and filtered pagination for profile

--- index.js
+++ index.js
@@ -53,26 +53,81 @@ for(let i = 0; i<17; i++){
     }
   ]})
 }
-
+for(let i = 0; i<17; i++){
+  POSTS.push({
+    postId: i, 
+    userId: "userId1", 
+    author: USERS[i].username,
+    timestamp: Date.now(), 
+    imgurl:`dummyimgurl${i}`, 
+    likes: i, 
+    title: "kaupungil testaas testikuval",
+    tags: ["kaupungil", "hengaas", "testtag"],
+    comments: [{
+      userId: USERS[i].userId,
+      commentId: `commentid${i}`,
+      username: USERS[i].username,
+      comment: "test 1",
+      timestamp: Date.now()
+    },
+    {
+      userId: USERS[i].userId,
+      commentId: `commentid${i}`,
+      username: USERS[i].username, 
+      comment: "test 2",
+      timestamp: Date.now()
+    },
+    {
+      userId: USERS[i].userId,
+      commentId: `commentid${i}`,
+      username: USERS[i].username, 
+      comment: "test 3",
+      timestamp: Date.now()
+    },
+    {
+      userId: USERS[i].userId,
+      commentId: `commentid${i}`,
+      username: USERS[i].username, 
+      comment: "test 4",
+      timestamp: Date.now()
+    }
+  ]})
+}
 app.use(cors({origin: "http://localhost:3000", credentials:true}));
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.json('Hello, World!');
-})
 
 app.get('/igposts/', paginatedResults(POSTS), (req, res) => {
   res.json({posts: res.paginatedResults, page: res.page, totalPages: res.totalPages})
 })
 
-app.get('/igposts/:postid', (req, res) => {
-
-  res.send(POSTS.get(req.params.postid))
-})
 
 app.get('/users/:userid', (req, res) => {
   res.json(USERS.find(u => u.userId === req.params.userid))
 })
+
+app.get('/igposts/:userid', paginatedResultsByUID(POSTS), (req, res) => {
+  res.json({posts: res.paginatedResults, page: res.page, totalPages: res.totalPages})
+})
+
+function paginatedResultsByUID(model){
+  return (req, res, next) => {
+    const filteredModel = model.filter(post => post.userId === req.params.userid)
+    const page = parseInt(req.query.page)
+    const limit = 5
+
+    const startIndex = (page - 1) * limit
+    const endIndex = page * limit
+
+    const totalPages = Math.ceil(filteredModel.length / limit)
+
+    const results = filteredModel.slice(startIndex, endIndex)
+    res.paginatedResults = results
+    res.page = page
+    res.totalPages = totalPages
+    next()
+  }
+}
 
 function paginatedResults(model){
   return (req, res, next) => {
